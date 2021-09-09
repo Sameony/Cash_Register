dec27821289274bde378191a0a5c916dfe3f567e
Sameony <iam2001sam@gmail.com>
2021-09-09 22:32:59 +0530

    refact: Efficient input from html switched from single input to input array

--- index.js
+++ index.js
@@ -5,19 +5,39 @@ const error_message = document.querySelector("#error-message");
 
 error_message.style.display="none";
 
+//Register
+const no_of_notes = document.querySelectorAll("#no-of-notes");
+
+const ourNotes = [1,5,10,20,50,100,500,2000];
+
+function calculateChange(change)
+{
+    for(var i=ourNotes.length-1;i>=0;i++)
+    {
+        ourNotes[i]=Math.trunc(change/)
+    }
+    
+
+}
+
 
 function clickEventHandler(){
     if(bill_amount.value < 0){
         errorHandler("Invalid bill amount");
     }
-    else if(cash_amount.value <= bill_amount)
+    else 
     {
-        errorHandler("The cash provided should at least be equal to the bill amount");
-    }
-    else{
-        const change_amount = cash_amount.value - bill_amount.value;
-        calculateChange(change_amount);
+        if(cash_amount.value > bill_amount)
+        {
+            const change_amount = cash_amount.value - bill_amount.value;
+            calculateChange(change_amount);
+            
+        }
+        else
+        {
+            errorHandler("The cash provided should at least be equal to the bill amount");
 
+        }
     }
 
 }
@@ -27,5 +47,6 @@ function errorHandler(Message)
     error_message.style.display="block";
     error_message.innerText = Message;
 }
+
 
 btn_check.addEventListener("click" ,clickEventHandler);
